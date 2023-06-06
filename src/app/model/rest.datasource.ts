234b88d182fcd8539a81c06e84590bd05393726b
Bernardo Brugnara Soccal <soccal2@yahoo.com.br>
2023-06-06 18:05:38 -0300

    page 163

---
+++ src/app/model/rest.datasource.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map, Observable } from "rxjs";
+import { Product } from "./product.model";
+import { Order } from "./order.model";
+
+const PROTOCOL = "http";
+const PORT = 3500;
+
+@Injectable()
+export class RestDataSource {
+   baseUrl: string;
+   auth_token?: string;
+
+   constructor(private http: HttpClient) {
+      this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
+   }
+
+   getProducts(): Observable<Product[]> {
+      return this.http.get<Product[]>(this.baseUrl + "products");
+   }
+   
+   saveOrder(order: Order): Observable<Order> {
+      return this.http.post<Order>(this.baseUrl + "orders", order);
+   }
+
+   authenticate(user: string, pass: string): Observable<boolean> {
+      return this.http.post<any>(this.baseUrl + "login", {
+         name: user, password: pass
+      }).pipe(map(response => {
+         this.auth_token = response.success ? response.token : null;
+         return response.success;
+      }));
+   }
+}
